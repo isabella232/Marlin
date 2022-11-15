0edf996e4608801ec23b5e069f093f4bfdcdd527
gIMpSTa <gimpsta@gmail.com>
2022-11-14 22:24:11 -0500

    Change bed size to 225x225

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1703,8 +1703,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 225
+#define Y_BED_SIZE 225
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
